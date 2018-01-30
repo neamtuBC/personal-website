360b79a3b42c2d41b896ef7dc1b187cb1ea1bb99
Neamtu <neamtu.bc@yahoo.com>
2018-01-30 13:53:44 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    WorkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
