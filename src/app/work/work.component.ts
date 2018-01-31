d28543834d5cdfb1e1b713c6454d72049730dc90
Neamtu <neamtu.bc@yahoo.com>
2018-01-31 15:48:55 +0200

    -work section

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkComponent implements OnInit {
 
+  allProjects = [
+    {
+      projectName : "First Project",
+      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectURL: "http://www.istoriaclujului.ml/",
+      projectTechnologies: ["unu", "doi", "trei"]
+      projectImage: "url('../../assets/img/project1.png')"
+    },
+    {
+      projectName : "Second Project",
+      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectURL: "http://www.istoriaclujului.ml/",
+      projectTechnologies: ["patru", "cinci", "sase"],
+      projectImage: "url('../../assets/img/project2.png')"
+    },
+    {
+      projectName : "Third Project",
+      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectURL: "http://www.istoriaclujului.ml/",
+      projectTechnologies: ["patru", "cinci", "sase"],
+      projectImage: "url('../../assets/img/project3.png')"
+    }
+  ]
+
+  currentProject = {}
+  flag = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeProject(index){
+    this.currentProject = this.allProjects[index];
+    this.flag = true;
+  }
 }
