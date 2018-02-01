228ed1522ac2245a1c671cf5f585d8a508a57445
Neamtu <neamtu.bc@yahoo.com>
2018-02-01 10:12:36 +0200

    -work section

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -12,7 +12,7 @@ export class WorkComponent implements OnInit {
       projectName : "First Project",
       projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
       projectURL: "http://www.istoriaclujului.ml/",
-      projectTechnologies: ["unu", "doi", "trei"]
+      projectTechnologies: ["unu", "doi", "trei"],
       projectImage: "url('../../assets/img/project1.png')"
     },
     {
@@ -28,7 +28,71 @@ export class WorkComponent implements OnInit {
       projectURL: "http://www.istoriaclujului.ml/",
       projectTechnologies: ["patru", "cinci", "sase"],
       projectImage: "url('../../assets/img/project3.png')"
-    }
+    },
+    {
+      projectName : "Third Project",
+      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectURL: "http://www.istoriaclujului.ml/",
+      projectTechnologies: ["patru", "cinci", "sase"],
+      projectImage: "url('../../assets/img/project3.png')"
+    },
+    {
+      projectName : "Third Project",
+      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectURL: "http://www.istoriaclujului.ml/",
+      projectTechnologies: ["patru", "cinci", "sase"],
+      projectImage: "url('../../assets/img/project3.png')"
+    },
+    {
+      projectName : "Third Project",
+      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectURL: "http://www.istoriaclujului.ml/",
+      projectTechnologies: ["patru", "cinci", "sase"],
+      projectImage: "url('../../assets/img/project3.png')"
+    },
+    {
+      projectName : "Third Project",
+      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectURL: "http://www.istoriaclujului.ml/",
+      projectTechnologies: ["patru", "cinci", "sase"],
+      projectImage: "url('../../assets/img/project3.png')"
+    },
+    {
+      projectName : "Third Project",
+      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectURL: "http://www.istoriaclujului.ml/",
+      projectTechnologies: ["patru", "cinci", "sase"],
+      projectImage: "url('../../assets/img/project3.png')"
+    },
+    {
+      projectName : "Third Project",
+      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectURL: "http://www.istoriaclujului.ml/",
+      projectTechnologies: ["patru", "cinci", "sase"],
+      projectImage: "url('../../assets/img/project3.png')"
+    },
+    {
+      projectName : "Third Project",
+      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectURL: "http://www.istoriaclujului.ml/",
+      projectTechnologies: ["patru", "cinci", "sase"],
+      projectImage: "url('../../assets/img/project3.png')"
+    },
+    {
+      projectName : "Third Project",
+      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectURL: "http://www.istoriaclujului.ml/",
+      projectTechnologies: ["patru", "cinci", "sase"],
+      projectImage: "url('../../assets/img/project3.png')"
+    },
+    {
+      projectName : "Third Project",
+      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectURL: "http://www.istoriaclujului.ml/",
+      projectTechnologies: ["patru", "cinci", "sase"],
+      projectImage: "url('../../assets/img/project3.png')"
+    },
+
   ]
 
   currentProject = {}
@@ -42,5 +106,9 @@ export class WorkComponent implements OnInit {
   changeProject(index){
     this.currentProject = this.allProjects[index];
     this.flag = true;
+  }
+
+  disableDescription(){
+    this.flag = false;
   }
 }
