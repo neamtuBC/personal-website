637674a3f139de1b3bb97294f20a04e43735a9de
Neamtu <neamtu.bc@yahoo.com>
2018-02-02 15:27:50 +0200

    -new images for projects -added images to project description -project description

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -24,31 +24,31 @@ export class WorkComponent implements OnInit {
   allProjects = [
     {
       projectName : "The history of Cluj",
-      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectDescription : "A responsive website for a nice presentation of the history of Cluj-Napoca. The website is designed as a timeline witch contains several sections.",
       projectURL: "http://www.istoriaclujului.ml/",
       projectTechnologies: ["Jekyll", "HTML", "SASS", "JavaScript", "jQuery"],
-      projectImage: "url('./assets/img/project1.png')"
+      projectImage: "url('./assets/img/istoriaclujului.jpg')"
     },
     {
       projectName : "Responsive Theme",
-      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectDescription : "A resposive theme where the owner can publish informations about his work, skills or his clients. The information displayed on the page can be easly edited just by modify a txt file. So, the owner does not have to know to code for editing his personal page.",
       projectURL: "https://neamtubc.github.io/ArtistPortfolio/",
       projectTechnologies: ["Jekyll", "HTML", "SASS", "JavaScript", "jQuery"],
-      projectImage: "url('./assets/img/project2.png')"
+      projectImage: "url('./assets/img/arthisttheme.jpg')"
     },
     {
       projectName : "A personal website",
-      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectDescription : "I developed this website as an exercise for practice some concepts as transitions, animations and more others. I folowed a tutotial from DevTips but I used my personal design.",
       projectURL: "https://neamtubc.github.io/PersonalWebsite/",
       projectTechnologies: ["Jekyll", "HTML", "SASS", "JavaScript", "jQuery"],
-      projectImage: "url('./assets/img/project3.png')"
+      projectImage: "url('./assets/img/apersonalwebsite.jpg')"
     },
     {
       projectName : "Canvas balls",
-      projectDescription : "Nullam nec lobortis felis. Morbi vel mattis lectus. Cras condimentum finibus molestie. Pellentesque sed blandit lectus. Ut tincidunt sapien risus, id fermentum lorem imperdiet non. Ut finibus sagittis justo. Pellentesque massa elit, posuere in erat a, pulvinar sagittis enim.",
+      projectDescription : "A little project where I tested the canvas properties. The website contains severals balls witch are changing their position permanently. The user can interact with this balls with the mouse cursor.",
       projectURL: "https://neamtubc.github.io/BulineleMiscatoarele/",
       projectTechnologies: ["HTML", "CSS", "JavaScript", "jQuery"],
-      projectImage: "url('./assets/img/project4.png')"
+      projectImage: "url('./assets/img/canvasballs.jpg')"
     },
   ]
 
